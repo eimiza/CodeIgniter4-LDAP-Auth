a80b99e38fa644824f1553f5d1cfb47449cbeae4
Eimiza Faisha Azmi <eimiza@ict.upsi.edu.my>
2022-10-09 20:55:55 +0800

    Create login.php
    Initial commit

---
+++ app/Views/auth/login.php
@@ -0,0 +1,9 @@
+<form method="post" action="/authenticate">
+    <label for="Username"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="username" required>
+
+    <label for="Password"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password" required>
+
+    <button type="submit">Login</button>
+</form>
